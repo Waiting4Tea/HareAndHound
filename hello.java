725d733b850d69826f3b6bba07caa27173e5cda9
Gudjon <gudjon@Gudjons-MBP.vodafone>
2018-04-06 21:27:04 +0000

    adding python code

---
+++ hello.java
@@ -0,0 +1,10 @@
+class hello {
+    public static void main(String[] args) {
+	String s = "Hello World";
+	prenta(s);
+    }
+
+    public static void prenta(String s) {
+	System.out.println(s);
+    }
+}
